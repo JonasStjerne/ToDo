aaaadd272caffbd91946fdfce85e658e27d42640
Jonas Stjerne <j.stjerne@live.dk>
2021-10-05 17:25:10 +0200

    Basic functions

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToDoComponent } from './componenets/to-do/to-do.component';
+import { MaterialModule } from './material/material.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ client/src/app/material/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+
+const MaterialComponents = [
+  MatSliderModule
+];
+
+@NgModule({
+  imports: [
+    MaterialComponents
+  ],
+  exports: [
+    MaterialComponents
+  ]
+})
+export class MaterialModule { }
